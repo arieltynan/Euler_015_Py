4b988e1a31f2825f7fcb7ff927133f72bf3a71b6
Ariel Tynan <tynan.a@northeastern.edu>
2022-01-23 16:44:59 -0700

    Cleaned up excess comments

--- Problem_015.py
+++ Problem_015.py
@@ -4,7 +4,7 @@
 # Lattice paths
 import math
 
-# Print Pascal's Triangle in Java 
+# Print Pascal's Triangle
 from math import factorial 
   
 # input n
@@ -19,9 +19,3 @@ tot = 0
 for i in nums:
     tot = tot + i**2
 print(tot)
-
-# Lee solution
-#from math import comb
-#
-#SIZE = 20
-#print(comb(SIZE+SIZE, SIZE))
